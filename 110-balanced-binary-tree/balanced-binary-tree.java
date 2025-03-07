af9e7f2f1afecb56c39914c95229ce48693b60ab
samikshagaiki <samikshagaiki@gmail.com>
2025-03-07 18:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (22.46%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -17,6 +17,7 @@ class Solution {
     public boolean isBalanced(TreeNode root) {
         return height(root) != -1;
     }
+    //
 
     private int height(TreeNode node) {
         if (node == null) return 0;
